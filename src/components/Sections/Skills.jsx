9d2d414de044b7b8eafb82051dcfe439efc7b97a
Nimal-Dev <nimalprince.dev@gmail.com>
2025-03-10 19:45:43 +0530

    Added Skills section

--- src/components/Sections/Skills.jsx
+++ src/components/Sections/Skills.jsx
@@ -1,21 +1,103 @@
 import React from "react";
+import reactpic from "../../assets/Skills/react.png"
+import html from "../../assets/Skills/html.png"
+import css from "../../assets/Skills/css.png"
+import bootstrap from "../../assets/Skills/bootstrap.png"
+import dart from "../../assets/Skills/dart.png"
+import flutter from "../../assets/Skills/flutter.png"
+import git from "../../assets/Skills/git.png"
+import github from "../../assets/Skills/github.png"
+import js from "../../assets/Skills/js.png"
+import mongodb from "../../assets/Skills/mongodb.png"
+import mysql from "../../assets/Skills/mysql.png"
+import nodejs from "../../assets/Skills/nodejs.png"
+import sqllite from "../../assets/Skills/sqllite.png"
+import django from "../../assets/Skills/django.png"
+import arduino from "../../assets/Skills/arduino.png"
 
 
-function Skills(){
-    return(
+function Skills() {
+    return (
         <>
-        <section id="Skills_Section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h2 className="text-center">Skills</h2>
-                        <div>
-                            
+            <section id="Skills_Section">
+                <div className="container">
+
+                    <h2 className="text-center skill-text">Skills</h2>
+                    
+                        <div className="skills container">
+                            <ul>
+
+                                <li className="skills-card">
+                                    <h4>React</h4>
+                                    <img src={reactpic} className="skills-logo" alt="React" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>HTML5</h4>
+                                    <img src={html} className="skills-logo" alt="html" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>JavaScript</h4>
+                                    <img src={js} className="skills-logo" alt="js" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>CSS</h4>
+                                    <img src={css} id="css" className="skills-logo" alt="CSS" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>BOOTSTRAP</h4>
+                                    <img src={bootstrap} className="skills-logo" alt="bootstrap" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>NodeJS</h4>
+                                    <img src={nodejs} className="skills-logo" alt="nodejs" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>GITHUB</h4>
+                                    <img src={github} className="skills-logo" alt="github" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>GIT</h4>
+                                    <img src={git} className="skills-logo" alt="git" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>MongoDB</h4>
+                                    <img src={mongodb} className="skills-logo" alt="MongoDB" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>Dart</h4>
+                                    <img src={dart} className="skills-logo" alt="Dart" />
+                                </li>
+
+                                <li className="skills-card">
+                                    <h4>Flutter</h4>
+                                    <img src={flutter} className="skills-logo" alt="Flutter" />
+                                </li>
+
+                                <li className="skills-card">
+                                    <h4>MySQL</h4>
+                                    <img src={mysql} className="skills-logo" alt="MySQL" />
+                                </li>
+
+                                <li className="skills-card">
+                                    <h4>SQLite</h4>
+                                    <img src={sqllite} className="skills-logo" alt="SQLite" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>Django</h4>
+                                    <img src={django} className="skills-logo" alt="Django" />
+                                </li>
+                                <li className="skills-card">
+                                    <h4>Arduino</h4>
+                                    <img src={arduino} className="skills-logo" alt="Arduino" />
+                                </li>
+
+                            </ul>
                         </div>
-                    </div>
+                    
+
+
                 </div>
-            </div>
-        </section>
+            </section>
         </>
     )
 }
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,62 +4,68 @@ import './navbar.css'
 
 
 function Navbar() {
-    return(
-      <nav class="navbar navbar-dark fixed-top custom-navbar">
-  <div class="container-fluid">
-    {/* <!-- Mobile: Brand on right, toggle on left --> */}
-    <div class="d-flex d-lg-none w-100">
-      <button class="navbar-toggler order-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand order-2 ms-auto" href="#">Nimal Prince</a>
-    </div>
+  return (
+    <nav class="navbar navbar-dark fixed-top custom-navbar">
+      <div class="container-fluid">
+        {/* <!-- Mobile: Brand on right, toggle on left --> */}
+        <div class="d-flex d-lg-none w-100">
+          <button class="navbar-toggler order-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand offcanvas-title order-2 ms-auto" href="#">Nimal Prince</a>
+        </div>
 
-    {/* <!-- Desktop: Centered content --> */}
-    <div class="d-none d-lg-flex justify-content-center w-100">
-      <ul class="navbar-nav d-flex flex-row">
-        <li class="nav-item mx-3">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item mx-3">
-          <a class="nav-link" href="#About_Section">About</a>
-        </li>
-        <li class="nav-item mx-3">
-          <a class="nav-link" href="#">Skills</a>
-        </li>
-        <li class="nav-item mx-3">
-          <a class="nav-link" href="#">Experience</a>
-        </li>
-        <li class="nav-item mx-3">
-          <a class="nav-link" href="#">Works</a>
-        </li>
-      </ul>
-    </div>
+        {/* <!-- Desktop: Centered content --> */}
+        <div class="d-none d-lg-flex justify-content-center w-100">
+          <ul class="navbar-nav d-flex flex-row">
+            <li class="nav-item mx-3">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#About_Section">About</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Skills</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Experience</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="#">Works</a>
+            </li>
+          </ul>
+        </div>
 
-    {/* <!-- Full-width offcanvas for mobile --> */}
-    <div class="offcanvas offcanvas-start text-bg-dark offcanvas-fullwidth" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Nimal Prince</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        {/* <!-- Glassmorphic offcanvas for mobile --> */}
+        <div class="offcanvas offcanvas-start glassmorphic offcanvas-fullwidth" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"><b>Nimal Prince</b></h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body d-flex align-items-center justify-content-center">
+            <ul class="navbar-nav text-center">
+              <li class="nav-item mb-4">
+                <a class="nav-link active fs-1" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item mb-4">
+                <a class="nav-link fs-1" href="#About_Section">About</a>
+              </li>
+              <li class="nav-item mb-4">
+                <a class="nav-link fs-1" href="#">Skills</a>
+              </li>
+              <li class="nav-item mb-4">
+                <a class="nav-link fs-1" href="#">Experience</a>
+              </li>
+              <li class="nav-item mb-4">
+                <a class="nav-link fs-1" href="#">Works</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#About_Section">About</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="#">Skills</a>
-        </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-    )
-    
+    </nav>
+  )
+
 }
 
 export default Navbar;
